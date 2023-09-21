ceedb710085531eb8c1eb0db76735a14108d031c
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-09-21 12:19:02 +0200

    Add css links

--- View/inscription.php
+++ View/inscription.php
@@ -3,35 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/connect.css">
     <script defer src="./js/authont.js"></script>
     <title>Register</title>
 </head>
 <body>
+<header>
+        <ul>
+            <li><a href="./">Home</a></li>
+        </ul>
+    </header>
 
-<form action="" method="post" id="formRegister">
-            
-            <h1>Register</h1>
-            
-            <label for="login">Login</label>
-            <input type="login" name="login" placeholder="Your login ...">
-            
-            <label for="firstName">FirstName</label>
-            <input type="text" name="firstName" placeholder="Your FirstName ...">
 
-            <label for="lastName">LastName</label>
-            <input type="text" name="lastName" placeholder="Your lastName ...">
-            
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="Your  Password ...">
-            
-            <label for="rePass">Confirm Password</label>
-            <input type="password" name="rePass" placeholder="Confirm Password ...">
-            
-            <button class="btn">Register</button>
-        </form>
+<main>
 
-        <div class="alert"></div>
+    <div class="alert"></div>
 
+    <form action="" method="post" id="formRegister">
+        
+        <h1>Register</h1>
+        
+        <label for="login">Login</label>
+        <input type="login" name="login" placeholder="Your login ...">
+        
+        <label for="firstName">FirstName</label>
+        <input type="text" name="firstName" placeholder="Your FirstName ...">
+        
+        <label for="lastName">LastName</label>
+        <input type="text" name="lastName" placeholder="Your lastName ...">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Your  Password ...">
+        
+        <label for="rePass">Confirm Password</label>
+        <input type="password" name="rePass" placeholder="Confirm Password ...">
+        
+        <button class="btn">Register</button>
+    </form>
+    
+    
+</main>
 
 </body>
 </html>
